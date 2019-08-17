024368518761a3fbc2b35c13e252fdb7cc40904c
Will Sutton <sutton_999@hotmail.com>
2019-08-17 20:53:50 +0100

    Update get_and_merge_new_tweets.R

--- get_and_merge_new_tweets.R
+++ get_and_merge_new_tweets.R
@@ -1,3 +1,29 @@
+setwd("icymi_email")
+# Load environment
+if(file.exists(".Renviron"){
+source(".Renviron")
+}
+
+# Load libraries
+library(rtweet)
+library(dplyr)
+library(tidyr)
+library(aws.s3)
+library(stringr)
+library(rmarkdown)
+
+# Load funcions
+source("function_timeline_tweets.R")
+
+# Log in to various apps
+source("login_twitter.R")
+
+# Get last run of data from s3 bucket
+if(file.exists("timeline_tweets.csv")){
+  file.remove("timeline_tweets.csv")
+}
+save_object("timeline_tweets.csv", bucket = "bucket-number-01")
+
 ## get new tweets and merge with old tweets
 new_tweets <- timeline_tweets(500)
 old_tweets <- read.csv("timeline_tweets.csv",stringsAsFactors = F)
