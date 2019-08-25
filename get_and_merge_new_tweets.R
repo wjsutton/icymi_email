27bcaaa707bbf51569e51bd61f608bbddb231752
Will Sutton <sutton_999@hotmail.com>
2019-08-25 21:08:02 +0100

    Update get_and_merge_new_tweets.R

--- get_and_merge_new_tweets.R
+++ get_and_merge_new_tweets.R
@@ -1,4 +1,6 @@
 setwd("icymi_email")
+options(stringsAsFactors=FALSE)
+readRenviron("/icymi_email/.Renviron")
 
 # Load libraries
 library(rtweet)
@@ -26,9 +28,11 @@ if(aws.s3::head_object("timeline_tweets.csv", bucket = s3_bucket)[1]==TRUE){
 
 ## get new tweets and merge with old tweets
 new_tweets <- timeline_tweets(500)
-old_tweets <- read.csv("timeline_tweets.csv",stringsAsFactors = F)
-old_tweets <- old_tweets[!old_tweets$status_id %in% new_tweets$status_id,]
-new_tweets <- rbind(new_tweets,old_tweets)
+if(file.exists("timeline_tweets.csv")){
+	old_tweets <- read.csv("timeline_tweets.csv",stringsAsFactors = F)
+	old_tweets <- old_tweets[!old_tweets$status_id %in% new_tweets$status_id,]
+	new_tweets <- rbind(new_tweets,old_tweets)
+}
 new_tweets <- unique(new_tweets)
 
 # remove tweets from previous weeks
@@ -48,5 +52,6 @@ aws.s3::put_object(tmp, object = "timeline_tweets.csv", bucket = s3_bucket)
 
 # at the end of the week produce report and send it to an s3 bucket
 if(format(Sys.Date(), "%A")=='Sunday'){
-  source("render_html_report.R")
+	write.csv(new_tweets,"timeline_tweets.csv",row.names = F)
+	source("render_html_report.R")
 }
